8d8c7c290405d35d13071f14e019f7686c44dd9c
pradeepbhattarai08 <pradeepbhattarai08@gmail.com>
2023-10-29 21:59:12 +0545

    saving...

---
+++ Secrets/env.js
@@ -0,0 +1,15 @@
+GOOGLE_ID=1018966291218-ide1cmnq93903g9ujvsstgsgvi33kd8u.apps.googleusercontent.com
+GOOGLE_CLIENT_SECRET=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
+MONGODB_URI=mongodb+srv://admin:admin@promptopia.ue1fsqi.mongodb.net/
+
+NEXTAUTH_URL=http://localhost:3000
+NEXTAUTH_URL_INTERNAL=http://localhost:3000
+NEXTAUTH_SECRET=9U5w9+0obqX/DmtSFIK3dWEwtoq9FfQje2KAbOUnMcA=
+
+
+
+
+
+
+
+User Your Own env contains from your account
